64f40797b5e873dbacc63c34b75893fa5a68c929
EniyavanThangaraj <eniyavant405@gmail.com>
2022-04-21 00:44:32 +0530

    project structure

---
+++ src/org/array/Array.java
@@ -0,0 +1,80 @@
+package org.array;
+
+public class Array {
+
+	public static void main(String[] args) {
+		
+		int a[]=new int[7];
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		a[3]=30;
+		a[4]=40;
+		a[5]=50;
+		a[6]=60;
+
+		System.out.println(a);
+		
+		
+		int length =a.length;
+		System.out.println(length);
+		
+		System.out.println("normal for loop");
+		for(int i=0;i<a.length;i++)
+		System.out.println(a[i]);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/org/array/Arraysumaverage.java
@@ -0,0 +1,116 @@
+package org.array;
+
+public class Arraysumaverage {
+
+	public static void main(String[] args) {
+		
+	int[] a=new int[] {10,20,30,40,50,60,70};	
+		
+		int b=0;
+		int length =a.length;
+		
+		for(int i=0;i<a.length;i++);{
+		b=b+a[21];
+		double average=b/a.length;
+		
+		System.out.println("average of array"+average);
+		
+		
+		
+		
+		
+		
+		
+		
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
